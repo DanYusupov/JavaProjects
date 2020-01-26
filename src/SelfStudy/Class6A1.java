691ece891b930bfd966242b8af4388ded3e5ccb4
Dan Yusupov <y.dan1985@gmail.com>
2020-01-26 11:21:37 -0500

    Commiting whole project

---
+++ src/SelfStudy/Class6A1.java
@@ -0,0 +1,16 @@
+package SelfStudy;
+
+public class Class6A1 {
+
+	public static void main(String[] args) {
+        
+		long lightSpeedPerSec = 1860001, nOfSecPerDay = 864001;
+		int Ndays = 100;
+		System.out.println("in 100 days,light will travel about"+ lightSpeedPerSec*nOfSecPerDay*Ndays+ "miles");
+		
+		
+		
+		
+	}
+
+}
